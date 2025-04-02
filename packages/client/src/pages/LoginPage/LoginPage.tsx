5a58adb8adad55d23c89b78a828d825797ad2e04
Vitaly Belik <vitaly.belik@gmail.com>
2025-04-02 07:12:59 +0300

    Сделана Login форма. Добавлена на страницу Login-а.

--- packages/client/src/pages/LoginPage/LoginPage.tsx
+++ packages/client/src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,20 @@
+import { Card, Typography } from 'antd'
+
+import { LoginForm } from '../../components/LoginForm'
+import React from 'react'
+import style from './LoginPage.module.scss'
+
+const { Title } = Typography
+
 export const LoginPage = () => {
-  return <div>Страница входа</div>
+  return (
+    <React.Fragment>
+      <Card className={style['login-page']}>
+        <Title level={2} className={style['login-title']}>
+          Login
+        </Title>
+        <LoginForm />
+      </Card>
+    </React.Fragment>
+  )
 }
