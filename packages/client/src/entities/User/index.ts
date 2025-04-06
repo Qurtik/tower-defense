495cfe055b533e82bd9bb638badaac79ec0bf9f8
Vladislav Selezov <vladislav.selezov@sgs.com>
2025-04-06 16:58:36 +0300

    Вынес форму профиля в отдельный компонент

--- packages/client/src/entities/User/index.ts
+++ packages/client/src/entities/User/index.ts
@@ -1,8 +1,17 @@
-import { useUserModel, authModel } from './model'
 import { AuthApi } from './api'
+import { useUserModel, authModel } from './model'
 import type { IUserData } from './types'
 
 import { LoginForm } from './ui/LoginForm'
 import { RegisterForm } from './ui/RegisterForm'
+import { ProfileForm } from './ui/ProfileForm'
 
-export { useUserModel, authModel, AuthApi, IUserData, LoginForm, RegisterForm }
+export {
+  useUserModel,
+  authModel,
+  AuthApi,
+  IUserData,
+  LoginForm,
+  RegisterForm,
+  ProfileForm,
+}
