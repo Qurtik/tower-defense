8c1ac47aed8edb0271d3775a8415aef8fa64e3d1
Vitaly Belik <vitaly.belik@gmail.com>
2025-04-13 11:47:37 +0300

    Установка jest + ts-jest

--- packages/client/src/app/App.test.tsx
+++ packages/client/src/app/App.test.tsx
@@ -1,7 +1,8 @@
-import App from './App'
 import { render, screen } from '@testing-library/react'
 
-/* const appContent = 'Вот тут будет жить ваше приложение :)' */
+import App from './App'
+
+const appContent = 'Вот тут будет жить ваше приложение :)'
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
@@ -9,6 +10,6 @@ global.fetch = jest.fn(() =>
 )
 
 test('Example test', async () => {
-  /*   render(<App />)
-  expect(screen.getByText(appContent)).toBeDefined() */
+  render(<App />)
+  expect(screen.getByText(appContent)).toBeDefined()
 })
