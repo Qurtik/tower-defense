62207e7ff88110c74c310073f6cd6c697dbbce03
Vitaly Kutuzov <raidzer93@gmail.com>
2025-05-07 15:48:17 +0300

    Откат ошибочных изменений httpService

--- packages/client/src/shared/api/httpService.ts
+++ packages/client/src/shared/api/httpService.ts
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
+const isServer = typeof window === 'undefined'
+
+const baseURL = isServer
+  ? process.env.API_PRACTICUM_URL
+  : import.meta.env.VITE_API_PRACTICUM_URL
+
 const httpService = axios.create({
-  baseURL:
-    process.env.API_PRACTICUM_URL || import.meta.env.VITE_API_PRACTICUM_URL,
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
