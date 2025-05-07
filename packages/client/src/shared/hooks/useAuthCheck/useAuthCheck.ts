d5c88dcd61173808532081e143040c1461b54129
Vitaly Kutuzov <raidzer93@gmail.com>
2025-05-07 15:34:04 +0300

    Исправлен конфликт с хуком, убрал обращение к sessionStorage на сервере

--- packages/client/src/shared/hooks/useAuthCheck/useAuthCheck.ts
+++ packages/client/src/shared/hooks/useAuthCheck/useAuthCheck.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooksRedux/hooksRedux'
 import { selectAuthLoading, selectIsAuthenticated } from '@/entities/User'
 import { getUserInfo } from '@/entities/User/model/thunks'
@@ -7,7 +7,12 @@ export const useAuthCheck = () => {
   const dispatch = useAppDispatch()
   const isAuthenticated = useAppSelector(selectIsAuthenticated)
   const isLoading = useAppSelector(selectAuthLoading)
-  const oauth = sessionStorage.getItem('oauth')
+  const [isClient, setIsClient] = useState(false)
+  const oauth = isClient && sessionStorage.getItem('oauth')
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   useEffect(() => {
     if (!isAuthenticated && !isLoading && !oauth) {
