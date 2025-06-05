16d9cc008328fdd5e7bb32bbfa4b2cdc6bb0b30d
TYury <yurytorgashin@simple-it.pro>
2025-06-05 14:35:16 +0300

    обновить туториал

--- packages/client/src/pages/GamePage/GamePage.tsx
+++ packages/client/src/pages/GamePage/GamePage.tsx
@@ -1,20 +1,10 @@
-import { Card, List, Typography } from 'antd'
 import styles from './GamePage.module.scss'
 import { Briefing } from '@/widgets/Briefing'
 import { TerminalButton } from '@/shared/ui/TerminalButton'
 import { useState } from 'react'
 import { GameCanvas } from '@/widgets/Game'
-
-const instructions = [
-  '► В начале раунда выбери ОДИН из 3 случайных апгрейдов',
-  '► Используй ограниченные рероллы для замены вариантов',
-  '► Каждая новая волна сильнее предыдущей',
-  '► Цель: продержаться максимальное число волн',
-]
-
+import { Tutorial } from '@/widgets/Tutorial'
 export const GamePage = () => {
-  const { Text } = Typography
-
   const [gameStarted, setGameStarted] = useState(false)
 
   if (gameStarted) {
@@ -25,18 +15,7 @@ export const GamePage = () => {
     <div className={styles.app}>
       <div className={styles.wrapper}>
         <div className={styles.howToPlay}>
-          <Card style={{ height: '100%' }}>
-            <List
-              header={<Text>{'Краткий инструктаж'.toUpperCase()}</Text>}
-              dataSource={instructions}
-              renderItem={item => (
-                <List.Item
-                  style={{ borderBottom: '1px dashed rgba(0, 255, 0, 0.3)' }}>
-                  <Text>{item}</Text>
-                </List.Item>
-              )}
-            />
-          </Card>
+          <Tutorial />
         </div>
 
         <div className={styles.briefing}>
--- packages/client/src/widgets/Briefing/ui/Briefing.tsx
+++ packages/client/src/widgets/Briefing/ui/Briefing.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import Typed from 'typed.js'
 import { Card, Typography } from 'antd'
 import styles from './Briefing.module.scss'
+import { briefingText } from '@/widgets/Briefing/data'
 
 const { Text } = Typography
 
@@ -10,22 +11,6 @@ export const Briefing = () => {
 
   useEffect(() => {
     if (!typedRef.current) return
-
-    const briefingText = [
-      '[Загрузка протокола «РОЙ»... Доступ разрешён]\n\n',
-      '*Командующий, это Центр Контроля Заражения.*\n\n',
-      'Активирован Чрезвычайный Протокол «РОЙ». ',
-      'Обнаружены сигнатуры ксеноморфных форм. ',
-      'Рой стремительно разрастается.\n\n',
-      '  ВАЖНО:\n',
-      '  - Заражение: 82% территории\n',
-      '  - Цель: Удержать позицию до прибытия подкрепления\n',
-      '  - Ресурсы: ОГРАНИЧЕНЫ\n\n',
-      '*Ваша база — последний рубеж перед сектором «Омега».*\n\n',
-      'Используйте улучшения стратегически —\n',
-      'каждый процент эффективности на счету.\n\n',
-      '[Автономный режим через 5... 4... 3...]',
-    ].join('')
 
     const typed = new Typed(typedRef.current, {
       strings: [briefingText],
